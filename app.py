2858a5d58622eb12dc661a548f97b10d1ea70c70
Sava Glisic <104223515+savaglisic@users.noreply.github.com>
2025-01-26 22:26:12 -0500

    old URI

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ db_password = os.environ.get('DB_PASS', 'Sava2290!')
 db_host = os.environ.get('DB_HOST', 'localhost')
 db_name = os.environ.get('DB_NAME', 'blueweb')
 
-app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+mysqlconnector://{db_user}:{db_password}@{db_host}:3306/{db_name}"
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+mysqlconnector://devuser:Sava2290!@localhost:3306/blueweb"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Initialize the database connection
